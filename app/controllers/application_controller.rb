3932358a7d25abd11438fb64e80157a72ed71c01
Alex Zhang <alexgzhang92@gmail.com>
2018-06-24 01:30:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     erb :super_hero
   end
   
-  post '/team' do
+  post '/teams' do
     @team = Team.new(params[:team])
     params[:team][:members].each do |member_info|
       Superhero.new(member_info)
